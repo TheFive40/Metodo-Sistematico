4ee5c43f338ae387b8170b72edd3b2911d8b2889
Jean Franco Boom Bolaño <55298889+TheFive40@users.noreply.github.com>
2022-10-15 16:46:57 -0500

    Update Sales_Holoideas.java

--- Sales_Holoideas.java
+++ Sales_Holoideas.java
@@ -2,29 +2,33 @@ package Quimica;
 
 import java.util.Scanner;
 
-public class Sales_Holoideas {
-public void Sales_Haloideas() {
+public class Sales_Holoideas { //NOMBRE DE LA CLASE 
+public void Sales_Haloideas() { //CREAMOS UN METODO SETTER
+	//PEDIMOS QUE EL USUARIO NOS INGRESE POR TECLADO EL COMPUESTO 
 	Scanner entrada = new Scanner(System.in);
 	System.out.println("Ingrese el compuesto");
 	String compuesto = entrada.nextLine();
-	
+	//ACA LE PEDIMOS QUE NOS INGRESE EL NUMERO DE ATOMOS DEL ELEMENTO NO METALICO
 	Scanner entrada2 = new Scanner(System.in);
-	System.out.println("Ingrese el número de atomos del elemento no metalico ");
+	System.out.println("Ingrese el nÃºmero de atomos del elemento no metalico ");
 	int atomos = entrada2.nextInt();
-	
+	//LE PEDIMOS QUE NOS INGRESE EL NUMERO DE ATOMOS DEL ELEMENTO METALICO
 	Scanner entrada4 = new Scanner(System.in);
-	System.out.println("Ingrese el número de atomos del elemento  metalico ");
+	System.out.println("Ingrese el nÃºmero de atomos del elemento  metalico ");
 	int metal = entrada4.nextInt();
 	
+	//POR ULTIMO LE PEDIMOS QUE NOS INGRESE  EL ESTADO DE OXIDACION DEL COMPUESTO EN NUMEROS ROMANOS
 	Scanner entrada3 = new Scanner(System.in);
-	System.out.println("Ingrese el estado de Oxidacion del compuesto en números romanos");
+	System.out.println("Ingrese el estado de Oxidacion del compuesto en nÃºmeros romanos");
 	String Oxidacion = entrada3.nextLine();
 	
+	//EVALUAMOS SI EN LA VARIABLE COMPUESTO QUE NOS INGRESO EL USUARIO POR TECLADO CONTIENE O SE TRATA DEL COMPUESTO CaCl ANIDADO A ESO
+	//EVALUAMOS SI LOS ATOMOS INGRESADOS POR EL USUARIO ES 1 Y SI EL NUMERO DEL ELEMENTO METALICO ES 1
 	if (compuesto.contains("CaCl") && atomos == 1 && metal == 1) {
-		System.out.println("Es un cloruro  de calcio");
+		System.out.println("Es un cloruro  de calcio"); //ENTONCES IMPRIMIMOS EN CONSOLA EL SIGUIENTE MENSAJE "ES UN CLORURO DE CALCIO"
 
-	}else if (compuesto.contains("CaCl") && atomos == 2 && metal == 1) {
-		System.out.println("Es un dicloruro  de calcio");
+	}else if (compuesto.contains("CaCl") && atomos == 2 && metal == 1) { //SI COMPUESTO CONTIENE CaCL Y EL NUMERO DE ATOMOS ES 2 Y EL NUMERO DEL ELEMENTO METALICO ES 1
+		System.out.println("Es un dicloruro  de calcio"); //ESTAMOS HABLANDO DE UN DI CLORURO DE CALCIO Y ASI HACEMOS CON LOS OTROS IF
 	}else if (compuesto.contains("CaCl") && atomos == 3 && metal == 1) {
 		System.out.println("Es un Tricloruro  de calcio");
 
